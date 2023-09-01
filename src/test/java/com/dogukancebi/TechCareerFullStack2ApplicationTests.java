4a38fd3d9bdd3d0a75541de3dde0ed97cdcfef0b
Dogukancebi <cebidogukan0@gmail.com>
2023-09-01 18:04:43 +0300

    Backend

---
+++ src/test/java/com/dogukancebi/TechCareerFullStack2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogukancebi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechCareerFullStack2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
